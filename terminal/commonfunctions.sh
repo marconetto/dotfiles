397293789bf283c38c6658c84819bea91e0f6ba8
Marco Netto <marco.netto@gmail.com>
2023-07-20 16:22:48 -0700

    move some shell functions to a common file

---
+++ terminal/commonfunctions.sh
@@ -0,0 +1,31 @@
+
+function ff(){
+   find . -iname "*$1*"
+}
+
+############### open vi with most recent file according to a pattern
+v(){
+
+    pattern=$1
+    cmd=""
+    for i in $@ ; do
+        cmd=$cmd" | grep -i $i"
+    done
+    # mrucache="$HOME/.cache/ctrlp/mru/cache.txt"
+    mrucache="$HOME/.vim_mru_files"
+    if [[ ! -f "$mrucache" ]] ; then
+        echo "vis has not found mru cache: $mrucache"
+        return
+    fi
+    cmd="cat $mrucache $cmd | grep -v '#' | head -n 1"
+    output=`eval $cmd`
+    if [[ !  -z  $output  ]] ;then
+        dir=$(dirname $output)
+
+        cd $dir > /dev/null
+        nvim $output
+    else
+        echo "no pattern found in mru cache"
+    fi
+    return 0
+}
