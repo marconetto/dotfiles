ddbad7a14aee35bebe3b1b55f4b9a756a59c3bda
Marco Netto <marco.netto@gmail.com>
2023-07-20 16:45:43 -0700

    updat some bashrc confi

--- terminal/bashprompt.sh
+++ terminal/bashprompt.sh
@@ -5,5 +5,7 @@ YELLOW="\[\e[33m\]"
 RESET="\[\e[0m\]"
 LIGHT_GREEN="\[\e[92m\]"
 
+COLOR_DARK_GREEN=64
+DARK_GREEN="\\033[38;5;${COLOR_DARK_GREEN}m"
 
-PS1="${GREEN}\u@\h:${BLUE}\w ${YELLOW}❯ ${RESET}"
+PS1="${GREEN}\u${DARK_GREEN}@${GREEN}\h:${BLUE}\w ${YELLOW}❯ ${RESET}"
