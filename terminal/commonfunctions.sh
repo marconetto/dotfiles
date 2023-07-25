474d545129256151233f11bd6d86742c860eaf22
Marco Netto <marco.netto@gmail.com>
2023-07-25 13:55:04 -0700

    fix tmux and nvim config for bash

--- terminal/commonfunctions.sh
+++ terminal/commonfunctions.sh
@@ -5,6 +5,37 @@ function install_fzf(){
    fi
 }
 
+function install_nvim(){
+    ORI_DIR=$HOME
+    if  [[ $PWD  != "." ]]; then
+        ORI_DIR=$PWD
+    fi
+    cd /tmp > /dev/null
+    if [[ ! -d $HOME/.local/nvim ]] ; then
+        if [[ `uname` == "Linux" ]]; then
+            # problem with GLIBC
+            #FILE=https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.tar.gz
+            #wget $FILE
+            #tar zxf nvim-linux64.tar.gz
+            #mv nvim-linux64 $HOME/.local/nvim
+            FILE=https://github.com/neovim/neovim/releases/download/v0.8.3/nvim.appimage
+            wget $FILE
+            chmod u+x nvim.appimage
+            echo "Extracting appimage for neovim..."
+            ./nvim.appimage --appimage-extract > /dev/null
+            mv ./squashfs-root/usr $HOME/.local/nvim/
+            rm -rf ./squashfs-root
+            rm ./nvim.appimage
+        else
+            FILE=https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-macos.tar.gz
+            wget $FILE
+            tar zxf nvim-macos.tar.gz
+            mv nvim-macos $HOME/.local/nvim
+        fi
+    fi
+    cd $ORI_DIR > /dev/null
+    export PATH=$HOME/.local/nvim/bin:$PATH
+}
 
 function ff(){
    find . -iname "*$1*"
