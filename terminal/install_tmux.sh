5ffad29d4b9a6de601f65c8ad0a2e51a76345dff
Marco Netto <marco.netto@gmail.com>
2023-09-06 13:19:01 -0700

    fix tmux installer

--- terminal/install_tmux.sh
+++ terminal/install_tmux.sh
@@ -21,12 +21,13 @@ if [ $(has_appimage_support) -eq "1" ] ; then
     curl -s https://api.github.com/repos/nelsonenzo/tmux-appimage/releases/latest \
     | grep "browser_download_url.*appimage" \
     | cut -d : -f 2,3 \
-    | tr -d \" \
-    | wget -qi - \
-    && chmod +x tmux.appimage
+    | tr -d \" > /tmp/listtmux.url
+    xargs -n 1 curl -OL < /tmp/listtmux.url
+    chmod +x tmux.appimage
 
     ## optionaly, move it into your $PATH
     mv tmux.appimage $HOME/.local/bin/tmux
+    rm -f /tmp/listtmux.url
 else
     echo "cannot use appimage for tmux"
     echo "trying miniconda"
