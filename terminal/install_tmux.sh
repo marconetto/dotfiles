d2063afb7fb779f2a1b76cd3c7975958fa1dd7b0
Marco Netto <marco.netto@gmail.com>
2023-09-06 13:08:50 -0700

    fix tmux installer

--- terminal/install_tmux.sh
+++ terminal/install_tmux.sh
@@ -32,7 +32,7 @@ else
     echo "trying miniconda"
 
     cd /tmp
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
     sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/.miniconda
 
     eval "$($HOME/.miniconda/bin/conda shell.bash hook)"
