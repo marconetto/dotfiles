250527674d9624f128dc63eb5df6e069a2b4d052
Marco Netto <marco.netto@gmail.com>
2023-10-14 01:53:37 -0700

    add menubar for wezterm

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -8,15 +8,176 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+-- hide new tab button
+config.show_new_tab_button_in_tab_bar = false
+-- config.tab_bar_appearance = "Fancy"
+
+--  hide close tab button
+config.use_fancy_tab_bar = false
+
+local SOLID_LEFT_ARROW = ' '
+local SOLID_RIGHT_ARROW = ' '
+wezterm.on(
+    'format-tab-title',
+    function(tab, tabs, panes, config, hover, max_width)
+        local edge_background = '#2C323B'
+        local background = '#242a33'
+        local foreground = '#808080'
+
+        if tab.is_active then
+            background = '#242a33'
+            foreground = '#5485c0'
+        else
+            background = '#333941'
+            foreground = '#777777'
+        end
+
+        local edge_foreground = background
+
+        -- ensure that the titles fit in the available space,
+        -- and that we have room for the edges.
+        local title = wezterm.truncate_right(tab.active_pane.title, max_width + 2)
+        local zoomed = '  '
+        local zz
+        if tab.active_pane.is_zoomed then
+            zoomed = ' ■'
+        end
+
+        return {
+            { Attribute = { Italic = false } },
+            { Attribute = { Underline = "None" } },
+            -- { Attribute = { Underline = "Dotted" } },
+            { Background = { Color = edge_background } },
+            { Foreground = { Color = edge_foreground } },
+            { Attribute = { Underline = "None" } },
+            { Text = SOLID_LEFT_ARROW },
+            { Foreground = { Color = edge_foreground } },
+            { Background = { Color = edge_background } },
+            { Text = '███' },
+            { Attribute = { Underline = "None" } },
+            { Background = { Color = background } },
+            { Foreground = { Color = foreground } },
+            { Attribute = { Underline = "None" } },
+            { Text = title },
+            { Text = zoomed },
+            { Attribute = { Underline = "None" } },
+            { Foreground = { Color = edge_foreground } },
+            { Background = { Color = edge_background } },
+            { Text = '█' },
+            { Background = { Color = edge_background } },
+            { Foreground = { Color = edge_foreground } },
+            { Text = SOLID_RIGHT_ARROW },
+        }
+    end)
+
+
+config.tab_max_width = 20
+
+-- function tab_title(tab_info)
+--     local title = tab_info.tab_title
+--     -- if the tab title is explicitly set, take that
+--     if title and #title > 0 then
+--         return title
+--     end
+--     -- Otherwise, use the title from the active pane
+--     -- in that tab
+--     return tab_info.active_pane.title
+-- end
+--
+-- wezterm.on(
+--     'format-tab-title',
+--     function(tab, tabs, panes, config, hover, max_width)
+--         local title = tab_title(tab)
+--         if tab.is_active then
+--             return {
+--                 { Background = { Color = 'blue' } },
+--                 { Text = ' ' .. title .. ' ' },
+--             }
+--         end
+--         return title
+--     end
+-- )
+
 config.color_scheme = 'SpaceGray Eighties'
 config.colors = {
     foreground = '#a9b1d6',
     background = '#2C323B',
     selection_fg = '#cccccc',
     selection_bg = '#4466aa',
-    split = "#555555",
+    split = "#444444",
     -- split = "#4E96E2"
+
+    tab_bar = {
+        background = "#2C323B",
+        active_tab = {
+            bg_color = "#2C323B",
+            fg_color = "#C8C093",
+            -- underline = "Single",
+        },
+        inactive_tab = {
+            bg_color = "#2C323B",
+            fg_color = "#727169",
+            italic = false,
+        },
+        -- inactive_tab_hover = {
+        --     bg_color = "#2A2A37",
+        --     fg_color = "#DCD7BA",
+        -- },
+        -- new_tab = {
+        --     bg_color = "#ff0000",
+        --     fg_color = "#727169",
+        -- },
+        -- new_tab_hover = {
+        --     bg_color = "#2A2A37",
+        --     fg_color = "#DCD7BA",
+        -- },
+    },
 }
+
+
+-- wezterm.on(
+--     'format-tab-title',
+--     function(tab, tabs, panes, config, hover, max_width)
+--         local title = #tab.tab_title > 0 and tab.tab_title or tab.active_pane.title
+--
+--         return {
+--             { Text = title },
+--         }
+--     end
+-- )
+
+-- wezterm.on(
+--     'format-tab-title',
+--     function(tab, tabs, panes, config, hover, max_width)
+--         local zoomed = ''
+--         if tab.active_pane.is_zoomed then
+--             zoomed = ' [Z]'
+--         end
+--
+--         if tab.is_active then
+--             return {
+--                 { Attribute = { Intensity = "Bold" } },
+--                 { Text = ' ' .. tab.tab_index + 1 .. ' ' .. tab.active_pane.title .. zoomed .. ' ' },
+--             }
+--         end
+--         return ' ' .. tab.tab_index + 1 .. ' ' .. tab.active_pane.title .. zoomed .. ' '
+--     end
+-- )
+
+-- wezterm.on("format-tab-title", function(tab)
+--     local tab_index = tab.tab_index + 1
+--     local tab_title = tab.active_pane.title
+--     local user_title = tab.active_pane.user_vars.panetitle
+--
+--     if user_title ~= nil and #user_title > 0 then
+--         tab_title = user_title
+--     end
+--
+--     return {
+--         { Text = "    " .. tab_title .. "    " },
+--         -- { Text = " " .. tab_index .. ": " .. tab_title .. "" },
+--     }
+-- end)
 
 config.window_padding = {
     left = '0.2cell',
@@ -29,7 +190,7 @@ config.window_decorations = "NONE | RESIZE"
 config.adjust_window_size_when_changing_font_size = false
 config.bold_brightens_ansi_colors = "No"
 config.line_height = 1.00
-config.font_size = 12
+config.font_size = 13
 
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
 -- test font: wezterm ls-fonts --text "❯    "
@@ -45,6 +206,14 @@ config.max_fps = 144
 config.animation_fps = 144
 config.front_end = "WebGpu"
 config.webgpu_power_preference = "HighPerformance"
+
+-- animation_fps = 60,
+--    max_fps = 60,
+--    front_end = 'WebGpu',
+--    webgpu_power_preference = 'HighPerformance',
+
+
+
 -- config.enable_kitty_keyboard = false
 -- config.scrollback_lines = 9999
 -- config.hide_tab_bar_if_only_one_tab = true
@@ -175,6 +344,67 @@ config.keys = {
 config.disable_default_key_bindings = true
 
 config.audible_bell = 'Disabled'
+
+
+------------------- tab bar -----------------------------
+-- config.tab_bar_style = {
+--     window_hide = wezterm.format({
+--         { Background = { Color = "#ff161D" } },
+--         { Foreground = { Color = "#727169" } },
+--         { Text = " _ " },
+--     }),
+--     window_hide_hover = wezterm.format({
+--         { Background = { Color = "#2A2A37" } },
+--         { Foreground = { Color = "#DCD7BA" } },
+--         { Text = " _ " },
+--     }),
+--     window_maximize = wezterm.format({
+--         { Background = { Color = "#16161D" } },
+--         { Foreground = { Color = "#727169" } },
+--         { Text = " [] " },
+--     }),
+--     window_maximize_hover = wezterm.format({
+--         { Background = { Color = "#2A2A37" } },
+--         { Foreground = { Color = "#DCD7BA" } },
+--         { Text = " [] " },
+--     }),
+--     window_close = wezterm.format({
+--         { Background = { Color = "#16161D" } },
+--         { Foreground = { Color = "#727169" } },
+--         { Text = " X " },
+--     }),
+--     window_close_hover = wezterm.format({
+--         { Background = { Color = "#2A2A37" } },
+--         { Foreground = { Color = "#DCD7BA" } },
+--         { Text = " X " },
+--     }),
+-- }
+
+-- config.window_frame = {
+--     font = config.font,
+--     font_size = config.font_size,
+--     inactive_titlebar_bg = "#ff161D",
+--     active_titlebar_bg = "#ff0000",
+--     inactive_titlebar_fg = "#727169",
+--     active_titlebar_fg = "#aaffaa",
+--     inactive_titlebar_border_bottom = "#54546D",
+--     active_titlebar_border_bottom = "#54546D",
+--     button_fg = "#727169",
+--     button_bg = "#16161D",
+--     button_hover_fg = "#DCD7BA",
+--     button_hover_bg = "#2A2A37",
+--
+--     border_left_width = "0.25cell",
+--     border_right_width = "0.25cell",
+--     border_bottom_height = "0.1cell",
+--     border_top_height = "0.1cell",
+--     border_left_color = "#16161D",
+--     border_right_color = "#16161D",
+--     border_bottom_color = "#1616ff",
+--     border_top_color = "#16161D",
+-- }
+------------------- tab bar -----------------------------
+
 
 return config
 -- ----------------------------------------------------------------------------
