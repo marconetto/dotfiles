b07af5e3f99fc7b2a716be8da0b9777aa8900c33
Marco Netto <marco.netto@gmail.com>
2025-01-10 07:38:09 -0800

    update sketchybar

--- os/sketchybar/plugins/aerospace.sh
+++ os/sketchybar/plugins/aerospace.sh
@@ -3,8 +3,8 @@
 # make sure it's executable with:
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 # ICON='●'
-ICON='󱓻'
-ICON="$1."
+# ICON='󱓻'
+# ICON="$1."
 
 all=$(aerospace list-windows --workspace "$1" | awk -F '|' '{gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}')
 
@@ -20,6 +20,9 @@ for app in "${apps[@]}"; do
     "Preview")
         icon=""
         ;;
+    "System Settings")
+        icon=""
+        ;;
     "WezTerm")
         icon=""
         ;;
@@ -29,10 +32,19 @@ for app in "${apps[@]}"; do
     "Microsoft Teams")
         icon="󰻞"
         ;;
+    "Microsoft PowerPoint")
+        icon="󰈨"
+        ;;
+    "Microsoft Word")
+        icon="󰈭"
+        ;;
+    "Microsoft Excel")
+        icon="󰈜"
+        ;;
     "Microsoft AutoUpdate")
         # do not show this annoying app
-        icon=""
         # icon="󰚰"
+        icon=""
         ;;
     *)
         icon="󱤆"
@@ -43,16 +55,23 @@ done
 
 # Trim leading space and display the result
 icons=$(echo "$icons" | xargs)
-echo "$icons"
-
-echo "$all"
-echo "$FOCUSED_WORKSPACE $1 $NAME"
+# echo "$icons"
+# echo "$FOCUSED_WORKSPACE $1 $NAME"
 ICON="$icons"
 ICON="$1 $icons"
+
+if [ ! -z "$FOCUSED_WORKSPACE" ]; then
+    echo "$FOCUSED_WORKSPACE" >~/.last_focused_workspace
+else
+    FOCUSED_WORKSPACE=$(cat ~/.last_focused_workspace)
+fi
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set "$NAME" background.drawing=on icon="$ICON" icon.color='0xffFFA500' icon.font.size=13 #label="$1" label.drawing=on
+    sketchybar --set "$NAME" background.drawing=on icon="$ICON" background.color=0x99050505 icon.color='0xffFFA500' icon.font.size=13 #label="$1" label.drawing=on
     # sketchybar --set "$NAME" background.drawing=off icon="$ICON" icon.color='0xffdfbf8e'
 else
-    sketchybar --set "$NAME" background.drawing=on icon="$ICON" icon.color='0x99dfbf8e' icon.font.size=13
+    sketchybar --set "$NAME" background.drawing=on icon="$ICON" background.color=0x77050505 icon.color='0x99dfbf8e' icon.font.size=13
     # sketchybar --set "$NAME" background.drawing=off icon='○'
 fi
+# echo "A=$A"
+# echo "---> $1 $FOCUSED_WORKSPACE $AEROSPACE_FOCUSED_WORKSPACE"
